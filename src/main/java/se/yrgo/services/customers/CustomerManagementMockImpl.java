e86b53f3af1979e4185c440660e9301440bd1451
Kod-Draken <m.dackhammar@gmail.com>
2025-04-17 16:30:51 +0200

    initial commit

---
+++ src/main/java/se/yrgo/services/customers/CustomerManagementMockImpl.java
@@ -0,0 +1,75 @@
+package se.yrgo.services.customers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.Predicate;
+
+import se.yrgo.domain.Call;
+import se.yrgo.domain.Customer;
+
+public class CustomerManagementMockImpl implements CustomerManagementService {
+	private HashMap<String,Customer> customerMap;
+
+	public CustomerManagementMockImpl() {
+		customerMap = new HashMap<String,Customer>();
+		customerMap.put("OB74", new Customer("OB74" ,"Fargo Ltd", "some notes"));
+		customerMap.put("NV10", new Customer("NV10" ,"North Ltd", "some other notes"));
+		customerMap.put("RM210", new Customer("RM210" ,"River Ltd", "some more notes"));
+	}
+
+
+	@Override
+	public void newCustomer(Customer newCustomer) {
+		customerMap.put(newCustomer.getCustomerId(), newCustomer);
+	}
+
+	@Override
+	public void updateCustomer(Customer changedCustomer) {
+		customerMap.put(changedCustomer.getCustomerId(), changedCustomer);
+
+	}
+
+	@Override
+	public void deleteCustomer(Customer oldCustomer) {
+		customerMap.remove(oldCustomer.getCustomerId());
+
+	}
+
+	@Override
+	public Customer findCustomerById(String customerId) throws CustomerNotFoundException {
+		return customerMap.get(customerId);
+	}
+
+	@Override
+	public List<Customer> findCustomersByName(String name) {
+		List<Customer> customerList = customerMap.values().stream().toList();
+
+		for (Customer customer : customerList) {
+			if (!customer.getCompanyName().equals(name)) {
+				customerList.remove(customer);
+			}
+		}
+		return customerList;
+	}
+
+	@Override
+	public List<Customer> getAllCustomers() {
+		return customerMap.values().stream().toList();;
+	}
+
+	@Override
+	public Customer getFullCustomerDetail(String customerId) throws CustomerNotFoundException {
+		return customerMap.get(customerId);
+	}
+
+	@Override
+	public void recordCall(String customerId, Call callDetails) throws CustomerNotFoundException {
+		//First find the customer
+		Customer customer = customerMap.get(customerId);
+
+		//Call the addCall on the customer
+		customer.getCalls().add(callDetails);
+	}
+
+}
